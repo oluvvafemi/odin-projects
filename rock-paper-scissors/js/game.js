6c33c58172ece952673c41c6ae4b0f09f0b4627e
Chris Adebiyi <oluvvafemi@gmail.com>
2022-04-13 12:54:00 +0200

    Add buttons,event listeners and scores to the game

--- rock-paper-scissors/js/game.js
+++ rock-paper-scissors/js/game.js
@@ -28,17 +28,21 @@ function compareChoices(playerSelection, computerSelection){
 
     };
     let order = relation[playerSelection];
-    return (
-        (order[computerSelection]>order[playerSelection]) ?
-        `You Lose! ${computerSelection} beats ${playerSelection}` : 
-        `You Win! ${playerSelection} beats ${computerSelection}`
-    );
+    let info = '';
+    if (order[computerSelection]>order[playerSelection]){
+        updateDisplayScore(0,1);
+        info = `${computerSelection} beats ${playerSelection}`;
+    }else{
+        updateDisplayScore(1,0);
+        info = `${playerSelection} beats ${computerSelection}`;
+    }
+    return info;
 }
 
 function playOneRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    if (playerSelection === computerSelection)  return "It's a draw";
+    if (playerSelection === computerSelection)  return `You both played ${playerSelection}`;
     switch (playerSelection) {
         case "rock":
             return compareChoices(playerSelection, computerSelection);
@@ -51,10 +55,32 @@ function playOneRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    for (let i=0; i<5; i++){
-        let playerSelection = prompt("rock, paper or scissors - your choice", '');
-        let computerSelection = computerPlay();
-        console.log(playOneRound(playerSelection, computerSelection));
-    }
+function updateDisplayScore(playerwin=0, computerwin=0, gameover=false){
+    if (gameover) {playerScore=computerScore=0};
+    playerScore += playerwin;
+    computerScore += computerwin;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
 }
+
+let playerScore=0,
+    computerScore=0;
+
+const playerScoreDisplay = document.querySelector("#playerscore");
+const computerScoreDisplay = document.querySelector("#computerscore");
+updateDisplayScore();
+
+const buttons = document.querySelectorAll("button");
+const result = document.querySelector("#result");
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    let computerSelection = computerPlay();
+    result.textContent = playOneRound(button.dataset.playerselection, computerSelection);
+    if(computerScore >= 5){
+        updateDisplayScore(0,0,true);
+        console.log("Computer Wins the Game!");
+    } else if(playerScore >= 5) {
+        updateDisplayScore(0,0,true);
+        console.log("Player Wins the Game!");
+    }
+}));
